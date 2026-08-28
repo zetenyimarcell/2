57b8dde144e4811ce4019cbc02ecf72fa60f0fd7
zetenyimarcell <marcellzetenyi75@gmail.com>
2026-08-28 07:12:56 +0200

    Remove redundant plugin application in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,4 @@ plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("com.google.gms.google-services") // <--- EZT IS BE KELL TENNED IDE IS!
 }
