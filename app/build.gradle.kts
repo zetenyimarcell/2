d7c033321958e702f57fe91b7bde5df8f76f8fb0
zetenyimarcell <marcellzetenyi75@gmail.com>
2026-08-29 16:39:09 +0200

    Update plugin versions in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    id("com.android.application")
-    id("org.jetbrains.kotlin.android")
-    id("com.google.gms.google-services")
+    id("com.android.application") version "8.2.0"
+    id("org.jetbrains.kotlin.android") version "1.9.20"
+    id("com.google.gms.google-services") version "4.4.1"
 }
 
 android {
