d50345ba5ca23483be45cce0fb30b489881dfff5
zetenyimarcell <marcellzetenyi75@gmail.com>
2026-08-29 16:09:42 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.gms.google-services")
+id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+
 }
 
 android {
